b926a7853e4c40a2d05a81373df927cf79055ec0
Linh Pham <question+github@closedsrc.org>
2025-04-03 18:16:59 -0700

    Bump Bootstrap to 5.3.4 and tweak search top padding

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ export BACKUP_DIR=/u01/www/_backups/
 export JEKYLL_ENV=production
 
 eval "$(rbenv init -)"
-npm run build
+npm run all
 cp -r _site ${SITE_PARENT}${SITE_DIR}.new
 mv ${SITE_PARENT}${SITE_DIR} ${BACKUP_DIR}${SITE_DIR}-`date +%Y%m%d-%H%M%S`
 mv ${SITE_PARENT}${SITE_DIR}.new ${SITE_PARENT}${SITE_DIR}
